781d4501f5798f776bea0c609cea61f7eefd597a
s32740 <s32740@pjwstk.edu.pl>
2026-07-21 20:59:37 +0200

    feat: utils.py

--- ingest.py
+++ ingest.py
@@ -3,19 +3,12 @@ from PyPDF2 import PdfReader
 from sentence_transformers import SentenceTransformer
 from supabase import create_client
 from dotenv import load_dotenv
+from utils import chunk_text
 
 load_dotenv()
 
 supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
 model = SentenceTransformer("all-MiniLM-L6-v2")
-
-def chunk_text(text, chunk_size=500, overlap=50):
-    words = text.split()
-    chunks=[]
-    for i in range (0, len(words), chunk_size-overlap):
-        chunk = " ". join(words[i:i+chunk_size])
-        chunks.append(chunk)
-    return chunks
 
 def process_pdf(filepath):
     filename = os.path.basename(filepath)
